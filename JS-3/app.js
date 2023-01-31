f1ef7afe775f8edd0c30398fdadcc4cf6889d72c
Qaisar Abbas <122449496+qaisar786abbas@users.noreply.github.com>
2023-01-30 21:22:39 -0800

    added files

---
+++ JS-3/app.js
@@ -0,0 +1,23 @@
+//****CHAPTER-3
+//VARIABLES AND NUMBERS
+
+
+// Questio-no 1
+// var age = "I am 19 years old";
+// alert(age);
+
+//Question-no 2
+// var visit = "You have visited this site 7 times";
+// alert(visit);
+
+//Question-no 3
+// var birthYear = "My birth year is 2003";
+// document.write(birthYear);
+
+//Question-no 4
+// var visitor = '"Qaisar Order 3 t-shirt From www.Daraz.com" ';
+// document.write(visitor)
+        //  OR
+// document.write('" Qaisar Order 3 t-shirt From www.Daraz.com" ');
+
+
